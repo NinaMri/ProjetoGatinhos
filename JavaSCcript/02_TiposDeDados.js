0da3ea935952edb436f2997dec35df0d7eaa6b63
Nina Maria Rezende Januário <ninamriz02@gmail.com>
2025-11-26 21:07:13 -0300

    subindo projeto

---
+++ JavaSCcript/02_TiposDeDados.js
@@ -0,0 +1,36 @@
+//O JavaScript possui 8 tipos de dados, sendo 7 deles, os chamados de tipos primitivos
+
+//1 - tipo NUMBER - usado para números inteiros ou decimais 
+let idade = 25;
+let preco = 334.50;
+
+console.log(preco);
+console.log("--------------------------------")
+
+//2 - tipos STRING - usado para textos
+let nome = "Nina"
+let sobrenome = "Rezende"
+let frase = 'apenas um teste de aspas simples'
+let frase2 = `exmplo de string com crase ${nome}`
+let nomeCompleto = `${nome} ${sobrenome}`
+
+console.log(nome, sobrenome);
+console.log(frase2);
+console.log(nomeCompleto);
+
+//3 - tipo BOOLEAN - usado para valores lógicos e que só pode ter duas respostas possíveis (verdadeiro-true/falso-false)
+
+let maiorIdade = true
+let aprovado = false
+
+//4 - tipo UNDERFINED - usado para variáveris que não foram inicializadas, ou seja, não receberam nenhum valor 
+let x
+console.log(x);
+
+x = 5
+console.log(x);
+
+//5 - tipo NULL -
+let respostas = null
+
+//6 - tipo
